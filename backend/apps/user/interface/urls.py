e38a18ae11919bbcd149356bfc7918588a7d7dcb
Bertrand2808 <bertrand2808@icloud.com>
2025-08-01 11:55:11 +0200

    apply blake

--- backend/apps/user/interface/urls.py
+++ backend/apps/user/interface/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import SimpleRouter
+
 from .views import UserViewSet
 
 router = SimpleRouter()
-router.register('', UserViewSet, basename='user')
+router.register("", UserViewSet, basename="user")
 
 urlpatterns = router.urls
