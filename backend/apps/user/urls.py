6230f8f8b80673c0b9965139a99a9bb15d6facce
Bertrand RENAUDIN <91090835+Bertrand2808@users.noreply.github.com>
2025-09-18 15:03:32 +0200

    [Ticket-26][BACK] — New App accounts : modèle ProfessionalUser (+ admin, migrations, API onboarding) (#12)
    * feat: Implement ProfessionalUser model, serializers, views, and API endpoints for onboarding and profile management
    
    * backup db
    
    * feat: Update settings to load environment variables and configure database settings

--- backend/apps/user/urls.py
+++ backend/apps/user/urls.py
@@ -1,9 +1,13 @@
-# apps/users/urls.py
+# apps/user/urls.py
 from django.urls import include, path
+
+from .interface.views import OnboardingProfessionalView, ProfessionalUserMeView
 
 app_name = "user"
 
 urlpatterns = [
     # on inclut simplement les routes de interface/urls.py
     path("", include("apps.user.interface.urls")),
+    path("professional/me/", ProfessionalUserMeView.as_view(), name="professional-me"),
+    path("onboarding/professional/", OnboardingProfessionalView.as_view(), name="onboarding-professional"),
 ]
