2d3bb57b3ffa6ac2ac8815e8ed3029557b0c39b0
Bertrand RENAUDIN <91090835+Bertrand2808@users.noreply.github.com>
2025-09-19 10:42:17 +0200

    MERGE - Ticket-27 - add permission for professional user API endpoint (#16)
    * add permission for professional user API endpoint
    
    * apply black and isort

--- backend/apps/user/urls.py
+++ backend/apps/user/urls.py
@@ -1,8 +1,11 @@
 # apps/user/urls.py
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from .interface.views import OnboardingProfessionalView, ProfessionalUserMeView
+from .interface.views import OnboardingProfessionalView, ProfessionalUserMeView, ProfessionalUserViewSet
 
+router = DefaultRouter()
+router.register(r"professional", ProfessionalUserViewSet, basename="professional")
 app_name = "user"
 
 urlpatterns = [
@@ -11,3 +14,5 @@ urlpatterns = [
     path("professional/me/", ProfessionalUserMeView.as_view(), name="professional-me"),
     path("onboarding/professional/", OnboardingProfessionalView.as_view(), name="onboarding-professional"),
 ]
+
+urlpatterns += router.urls
