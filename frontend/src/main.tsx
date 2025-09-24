51619398c2cb6e5c40d20be77a041639fa034e29
Bertrand RENAUDIN <91090835+Bertrand2808@users.noreply.github.com>
2025-09-24 19:01:22 +0200

    MERGE - Front update (#22)
    * feat: Implement Navbar component with responsive design and authentication handling
    
    - Added Navbar component with links for user authentication (login, register, logout).
    - Created Navbar.module.css for styling using Tailwind CSS.
    - Integrated Navbar into LoginPage and RegisterPage components.
    - Added LoginPage.module.css and RegisterPage.module.css for respective page styles.
    - Implemented user redirection in LoginPage and RegisterPage based on authentication state.
    - Defined global styles in index.css, including custom font and color themes.
    
    * fix issue format

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './app/router';
 import { AuthProvider } from './app/providers/AuthProvider';
-import './index.css';
+
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
