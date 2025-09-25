9f58ad584a54761deaaf931c8f1492387e058b92
Bertrand RENAUDIN <91090835+Bertrand2808@users.noreply.github.com>
2025-09-25 10:10:42 +0200

    Bertrand2808 patch 1 (#24)
    * Create CODEOWNERS
    
    * Create test.md
    
    * Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -64,7 +64,7 @@ else:
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-jovyrahw%x-2w323nfg0$o@n+lz2ru5nca+f@#lyzwl5p3&+^%"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
