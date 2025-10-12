bed9acea313a038d450069169478ef5ce74fdc4c
Bertrand2808 <bertrand2808@icloud.com>
2025-10-12 21:12:10 +0200

    chore(release): bump version to 0.1.0

--- frontend/src/lib/constants/site.config.ts
+++ frontend/src/lib/constants/site.config.ts
@@ -6,4 +6,5 @@ export const SITE = {
   email: 'contact@freelansign.com',
   twitter: 'https://twitter.com/freelansign',
   github: 'https://github.com/freelansign',
+  version: '0.1.0',
 } as const;
